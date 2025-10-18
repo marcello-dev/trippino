6138c9d00f46f84893288e1f1b0bf06c67b1981c
marcello-dev <monachesi.m@live.com>
2025-10-18 10:08:29 +0000

    add systemd scripts

--- ops/setup.sh
+++ ops/setup.sh
@@ -11,14 +11,43 @@ chown -R trippino:trippino /home/trippino/.ssh
 mkdir /opt/trippino
 chown -R trippino:trippino /opt/trippino
 
-# As trippino user
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "$HOME/.nvm/nvm.sh"
-nvm install 22
-node -v 
-npm -v
-
+dnf install nodejs
 npm install -g http-server
 
+# As trippino
 cd /opt/trippino
 npm install --production
+
+# As root
+cat << EOL > /etc/systemd/system/trippino-api.service
+[Unit]
+Description=trippino-api
+After=syslog.target
+
+[Service]
+User=trippino
+ExecStart=node /opt/trippino/backend/index.js
+SuccessExitStatus=143
+q
+[Install]
+WantedBy=multi-user.target
+EOL
+systemctl daemon-reload
+systemctl enable trippino-api
+
+
+cat << EOL > /etc/systemd/system/trippino-frontend.service
+[Unit]
+Description=trippino-frontend
+After=syslog.target
+
+[Service]
+User=trippino
+ExecStart=npx http-server /opt/trippino/frontend -p 5000
+SuccessExitStatus=143
+
+[Install]
+WantedBy=multi-user.target
+EOL
+systemctl daemon-reload
+systemctl enable trippino-frontend
