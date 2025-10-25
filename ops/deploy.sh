6d4660a76d2d5d921a03e59b13f72a8bb9aa3538
marcello-dev <monachesi.m@live.com>
2025-10-25 00:34:41 +0000

    automate deployment commands

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,10 +1,13 @@
+echo "Archiving..."
 git archive --format=tar --prefix=trippino/ HEAD | gzip > /tmp/trippino.tar.gz
+echo "Deploying..."
 scp /tmp/trippino.tar.gz  trippino@api-as.apivenue.com:/tmp
-# ssh into vm as "trippino"
-sudo rm -r /opt/trippino
-sudo tar -xzf /tmp/trippino.tar.gz -C /opt
-sudo chown -R trippino:trippino /opt/trippino
-sed -i 's/http:\/\/localhost:4000/https:\/\/trippino-api.apivenue.com/g' /opt/trippino/frontend/index.html
-npm --prefix /opt/trippino install --omit=dev
-sudo systemctl restart trippino-api
-sudo systemctl restart trippino-frontend
+ssh trippino@api-as.apivenue.com "sudo rm -r /opt/trippino"
+ssh trippino@api-as.apivenue.com "sudo tar -xzf /tmp/trippino.tar.gz -C /opt"
+ssh trippino@api-as.apivenue.com "sudo chown -R trippino:trippino /opt/trippino"
+ssh trippino@api-as.apivenue.com "sed -i 's/http:\/\/localhost:4000/https:\/\/trippino-api.apivenue.com/g' /opt/trippino/frontend/index.html"
+ssh trippino@api-as.apivenue.com "npm --prefix /opt/trippino install --omit=dev"
+echo "Restarting services..."
+ssh trippino@api-as.apivenue.com "sudo systemctl restart trippino-api"
+ssh trippino@api-as.apivenue.com "sudo systemctl restart trippino-frontend"
+echo "Deployment complete"
