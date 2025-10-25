45571374bbe0a485a54f91bdf4da9b37fc12aa38
marcello-dev <monachesi.m@live.com>
2025-10-25 14:27:32 +0000

    simplify deploy script

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,12 +1,11 @@
 echo "Archiving..."
 git archive --format=tar --prefix=trippino/ HEAD | gzip > /tmp/trippino.tar.gz
 echo "Deploying..."
-scp /tmp/trippino.tar.gz  trippino@api-as.apivenue.com:/tmp
-ssh trippino@api-as.apivenue.com "sudo rm -r /opt/trippino"
-ssh trippino@api-as.apivenue.com "sudo tar -xzf /tmp/trippino.tar.gz -C /opt"
-ssh trippino@api-as.apivenue.com "sudo chown -R trippino:trippino /opt/trippino"
-ssh trippino@api-as.apivenue.com "npm --prefix /opt/trippino install --omit=dev"
+scp /tmp/trippino.tar.gz  trippino@apivenue.com:/tmp
+ssh trippino@apivenue.com "sudo rm -r /opt/trippino"
+ssh trippino@apivenue.com "sudo tar -xzf /tmp/trippino.tar.gz -C /opt"
+ssh trippino@apivenue.com "sudo chown -R trippino:trippino /opt/trippino"
+ssh trippino@apivenue.com "npm --prefix /opt/trippino install --omit=dev"
 echo "Restarting services..."
-#ssh trippino@api-as.apivenue.com "sudo systemctl restart trippino-api"
-#ssh trippino@api-as.apivenue.com "sudo systemctl restart trippino-frontend"
+ssh trippino@apivenue.com "sudo systemctl restart trippino-api"
 echo "Deployment complete"
